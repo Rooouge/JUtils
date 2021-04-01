77869e86c71446b2099abb567cf7f741d4a2b350
Andrea Rossi <andrea.rossi@besolutions.it>
2021-04-01 14:43:45 +0200

    EmptyPanel divided in ColoredPanel and TransparentPanel

--- src/jutils/gui/ColoredPanel.java
+++ src/jutils/gui/ColoredPanel.java
@@ -5,20 +5,13 @@ import java.awt.Color;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class EmptyPanel extends JPanel {
-
-	/**
-	 * Creates a transparent JPanel (with null background color)
-	 */
-	public EmptyPanel() {
-		this(null);
-	}
+public class ColoredPanel extends JPanel {
 	
 	/**
 	 * Creates a JPanel with the specified background color
 	 * @param color the background color
 	 */
-	public EmptyPanel(Color color) {
+	public ColoredPanel(Color color) {
 		super();
 		this.setBackground(color);
 	}
---
+++ src/jutils/gui/TransparentPanel.java
@@ -0,0 +1,14 @@
+package jutils.gui;
+
+
+@SuppressWarnings("serial")
+public class TransparentPanel extends ColoredPanel {
+
+	/**
+	 * Creates a JPanel with null background
+	 */
+	public TransparentPanel() {
+		super(null);
+	}
+
+}

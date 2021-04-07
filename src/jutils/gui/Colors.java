d1dbd6ca270c65df429a0ad196957dc1a3320739
Andrea Rossi <andrea.rossi@besolutions.it>
2021-04-07 18:15:19 +0200

    Added more classes from other projects

---
+++ src/jutils/gui/Colors.java
@@ -0,0 +1,32 @@
+package jutils.gui;
+
+import java.awt.Color;
+
+public class Colors {
+
+	public static final Color GRAY_32 = gray(32);
+	public static final Color GRAY_48 = gray(48);
+	public static final Color GRAY_64 = gray(64);
+	
+	
+	public static Color red(int amount) {
+		return new Color(amount, 0, 0);
+	}
+	
+	public static Color green(int amount) {
+		return new Color(0, amount, 0);
+	}
+	
+	public static Color blue(int amount) {
+		return new Color(0, 0, amount);
+	}
+	
+	public static Color gray(int amount) {
+		return new Color(amount, amount, amount);
+	}
+	
+	
+	public static Color convertRGBToARGB(Color color, int transparency) {
+		return new Color(color.getRed(), color.getGreen(), color.getBlue(), transparency);
+	}
+}
